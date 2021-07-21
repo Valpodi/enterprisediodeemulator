8d131801fc7417440aab72369eff064cb417af1e
Jim Matthews <jim.matthews@paconsulting.com>
2021-07-21 15:28:44 +0000

    ODED-180 run http server in container, create emulator starter that launches the interface

---
+++ src/Emulator/launch_emulator_with_interface.py
@@ -0,0 +1,17 @@
+# Copyright PA Knowledge Ltd 2021
+# For licence terms see LICENCE.md file
+
+import subprocess
+
+
+def start_emulator_with_interface():
+    subprocess.Popen(
+        f'docker run -v /var/run/docker.sock:/var/run/docker.sock '
+        f'           -v $(pwd):$(pwd)'
+        f'           -p 8081:8081 '
+        f'           -d emulatorinterface',
+        shell=True)
+
+
+if __name__ == "__main__":
+    start_emulator_with_interface()
