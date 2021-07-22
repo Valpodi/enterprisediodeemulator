7c6be0ebd8951c17c18fbfb6abe14e5288c5b7b4
Jim Matthews <jim.matthews@paconsulting.com>
2021-07-22 10:50:53 +0000

    ODED-180 remove duplicate dockerfile and update path for test script

--- scripts/integrationTestsDocker.sh
+++ scripts/integrationTestsDocker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 set -eux
 
-BUILDIMAGE=$(docker build -q -f src/test/Dockerfile .)
+BUILDIMAGE=$(docker build -q -f test/Dockerfile .)
 
 docker run -v /var/run/docker.sock:/var/run/docker.sock \
         -v "$(pwd)":"$(pwd)" \
