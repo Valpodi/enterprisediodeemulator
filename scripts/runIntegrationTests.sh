82d4bc4a28d4e466c462acd10392530e50a87cb2
Jim Matthews <jim.matthews@paconsulting.com>
2021-07-23 11:18:07 +0000

    ODED-180 remove script

--- scripts/runIntegrationTests.sh
+++ scripts/runIntegrationTests.sh
@@ -33,4 +33,4 @@ if [[ "$OUTPUT" != "$EXPECTED" ]]; then
 fi
 popd
 
-./scripts/runInterfaceIntegrationTests.sh
+python3 -m nose --with-xunit --xunit-file=test/interface_integration_test_results.xml test/mgmt_interface_integration_tests.py
--- scripts/runInterfaceIntegrationTests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -eux
-
-python3 -m nose --with-xunit --xunit-file=test/interface_integration_test_results.xml  test/mgmt_interface_integration_tests.py
-
