11a0057b4ec567fa642e793ff9ac28a99c3ac4f5
Janki Shah <janki.shah@paconsulting.com>
2021-07-27 14:21:32 +0000

    refactor test_power_on_endpoint

--- Emulator/launch_emulator_with_interface.py
+++ Emulator/launch_emulator_with_interface.py
@@ -8,6 +8,7 @@ def start_emulator_with_interface():
     subprocess.Popen(
         f'docker run -v /var/run/docker.sock:/var/run/docker.sock '
         f'           -v "$(pwd)":"$(pwd)"'
+        f'           --name interface'
         f'           -p 172.17.0.1:8081:8081 '
         f'           -d emulatorinterface /bin/bash -c "pushd $(pwd) && python3 /usr/src/app/http_server.py"',
         shell=True)
