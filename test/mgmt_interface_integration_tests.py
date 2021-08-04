7ad6dd06eb8ba57d26388719ddf47e96a5ba9905
Jim Matthews <jim.matthews@paconsulting.com>
2021-08-04 09:53:46 +0000

    use test helpers for wait for action

--- test/mgmt_interface_integration_tests.py
+++ test/mgmt_interface_integration_tests.py
@@ -7,7 +7,7 @@ import requests
 import json
 import time
 import threading
-
+from test_helpers import TestHelpers
 import launch_emulator
 from Emulator import launch_emulator_interface
 
@@ -21,7 +21,7 @@ class MgmtInterfaceIntegrationTests(unittest.TestCase):
         cls.start_interface_server()
         cls.save_port_config()
         try:
-            cls.wait_for_port(8081)
+            TestHelpers.wait_for_open_comms_ports("172.17.0.1", 8081, "zv")
         except TimeoutError as ex:
             print(f"Exception during setUpClass: {ex}")
             cls.clean_up_class()
@@ -36,25 +36,6 @@ class MgmtInterfaceIntegrationTests(unittest.TestCase):
     def start_interface_server(cls):
         cls.interface_server_thread = threading.Thread(target=launch_emulator_interface.start_interface, args=(8081,))
         cls.interface_server_thread.start()
-
-    @classmethod
-    def wait_for_port(cls, port_to_check, options="zv"):
-        cls.wait_for_action(lambda: (subprocess.call(
-            f"nc -{options} 172.17.0.1 {port_to_check} -w 1".split())), 0, f"port {port_to_check} should be open",
-                            delay=3)
-
-    @classmethod
-    def wait_for_action(cls, action, expected_result, message, delay=0, attempts=5):
-        condition_met = False
-        while not condition_met:
-            action_output = action()
-            condition_met = (action_output == expected_result)
-            attempts = attempts - 1
-            if attempts == 0:
-                raise TimeoutError("Failed waiting for: " + message)
-            time.sleep(delay)
-        if condition_met:
-            return action_output
 
     @classmethod
     def clean_up_class(cls):
@@ -85,30 +66,31 @@ class MgmtInterfaceIntegrationTests(unittest.TestCase):
         self.assertEqual(200, response.status_code)
 
     def test_power_on_endpoint(self):
-        self.assertRaises(TimeoutError, self.wait_for_port, 40001, "zvu")
+        self.assertRaises(TimeoutError, TestHelpers.wait_for_open_comms_ports, "172.17.0.1", 40001, "zvu")
+
         response = requests.post("http://172.17.0.1:8081/api/command/diode/power/on")
-        self.wait_for_port(40001, "zvu")
+        TestHelpers.wait_for_open_comms_ports("172.17.0.1", 40001, "zvu")
 
         self.assertEqual("completed", json.loads(response.text)['status'])
         self.assertEqual(200, response.status_code)
 
     def test_power_off_endpoint(self):
         launch_emulator.start_emulator("Emulator/config/portConfig.json")
-        self.wait_for_port(40001, "zvu")
+        TestHelpers.wait_for_open_comms_ports("172.17.0.1", 40001, "zvu")
 
         response = requests.post("http://172.17.0.1:8081/api/command/diode/power/off")
-        self.assertRaises(TimeoutError, self.wait_for_port, 40001, "zvu")
+        self.assertRaises(TimeoutError, TestHelpers.wait_for_open_comms_ports, "172.17.0.1", 40001, "zvu")
 
         self.assertEqual("completed", json.loads(response.text)['status'])
         self.assertEqual(200, response.status_code)
 
     def test_power_off_removes_emulator_container(self):
         requests.post("http://172.17.0.1:8081/api/command/diode/power/on")
-        self.wait_for_port(40001, "zvu")
+        TestHelpers.wait_for_open_comms_ports("172.17.0.1", 40001, "zvu")
         requests.post("http://172.17.0.1:8081/api/command/diode/power/off")
-        self.assertRaises(TimeoutError, self.wait_for_port, 40001, "zvu")
+        self.assertRaises(TimeoutError, TestHelpers.wait_for_open_comms_ports, "172.17.0.1", 40001, "zvu")
         requests.post("http://172.17.0.1:8081/api/command/diode/power/on")
-        self.wait_for_port(40001, "zvu")
+        TestHelpers.wait_for_open_comms_ports("172.17.0.1", 40001, "zvu")
 
     def test_update_config_endpoint(self):
         with open('Emulator/config/portConfig.json', 'r') as config_file:
@@ -117,14 +99,14 @@ class MgmtInterfaceIntegrationTests(unittest.TestCase):
         new_config["routingTable"][0]["ingressPort"] = 40002
 
         requests.post("http://172.17.0.1:8081/api/command/diode/power/on")
-        self.wait_for_port(40001, "zvu")
+        TestHelpers.wait_for_open_comms_ports("172.17.0.1", 40001, "zvu")
 
         response = requests.put("http://172.17.0.1:8081/api/config/diode",
                                 json=new_config,
                                 headers={"Content-Type": "application/json"})
         self.assertEqual("completed", json.loads(response.text)['status'])
 
-        self.wait_for_port(40002, "zvu")
+        TestHelpers.wait_for_open_comms_ports("172.17.0.1", 40002, "zvu")
 
 
 if __name__ == '__main__':
