0233a4e7bbd413844ec454f5b902bc53c28fd1b4
Janki Shah <janki.shah@paconsulting.com>
2021-08-09 15:53:57 +0000

    mock file exists check

--- Emulator/mgmt_interface_tests.py
+++ Emulator/mgmt_interface_tests.py
@@ -8,12 +8,13 @@ from mgmt_interface import Interface
 
 class MgmtInterfaceTests(unittest.TestCase):
     def test_do_config_get_returns_config_file(self):
+        Interface._file_exists = lambda file: True
         Interface._get_config_file = lambda: {
             "ingress": {},
             "egress": {},
             "routingTable": []
         }
-        response = Interface.do_config_get()
+        response = Interface().do_config_get()
 
         self.assertEqual({"ingress": {}, "egress": {}, "routingTable": []}, json.loads(response.get_data()))
         self.assertEqual(200, response.status_code)
@@ -33,6 +34,7 @@ class MgmtInterfaceTests(unittest.TestCase):
         self.assertEqual(200, response.status_code)
 
     def test_diode_get_schema_returns_config_schema(self):
+        Interface._file_exists = lambda file: True
         schema = {"properties": {"ingress": {"type": "object"},
                                  "egress": {"type": "object"},
                                  "routingTable": {"type": "array"}},
