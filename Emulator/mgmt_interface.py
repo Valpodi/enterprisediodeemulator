221b91ed8287f2f9d24af485f94324903b3064bd
Jim Matthews <jim.matthews@paconsulting.com>
2021-08-09 15:28:42 +0000

    refactor managment interface

--- Emulator/mgmt_interface.py
+++ Emulator/mgmt_interface.py
@@ -15,15 +15,36 @@ class Interface:
 
     @classmethod
     def do_config_get(cls):
-        if os.path.exists('Emulator/config/portConfig.json'):
-            return Response(json.dumps(cls._get_config_file()), 200)
+        return cls._check_file_and_action('Emulator/config/portConfig.json',
+                                          cls._get_config_file,
+                                          "Config file does not exist")
+
+    @classmethod
+    def _check_file_and_action(cls, file, action, error_message):
+        if cls._file_exists(file):
+            return Response(json.dumps(action()), 200)
         else:
-            return Response(json.dumps({"status": "Config file does not exist"}), 200)
+            return Response(json.dumps({"status": error_message}), 200)
+
+    @staticmethod
+    def _file_exists(filepath):
+        return os.path.exists(filepath)
 
     @classmethod
     def _get_config_file(cls):
         with open('Emulator/config/portConfig.json', 'r') as config_file:
             return json.loads(config_file.read())
+
+    @classmethod
+    def get_config_schema(cls):
+        return cls._check_file_and_action('Emulator/openapi/schema.json',
+                                          cls._get_schema_file,
+                                          "Schema file does not exist")
+
+    @classmethod
+    def _get_schema_file(cls):
+        with open('Emulator/openapi/schema.json', 'r') as schema_file:
+            return json.loads(schema_file.read())
 
     @classmethod
     def do_config_update(cls):
@@ -46,10 +67,9 @@ class Interface:
 
     @classmethod
     def do_power_on_procedure(cls):
-        if os.path.exists('Emulator/config/portConfig.json'):
-            return Response(json.dumps(cls._power_on_diode()), 200)
-        else:
-            return Response(json.dumps({"status": "Config file could not be found to power on diode"}), 200)
+        return cls._check_file_and_action('Emulator/config/portConfig.json',
+                                          cls._power_on_diode,
+                                          "Config file could not be found to power on diode")
 
     @classmethod
     def _power_on_diode(cls):
@@ -61,16 +81,7 @@ class Interface:
 
     @classmethod
     def _power_off_diode(cls):
-        return {"status": {0: "completed"}.get(subprocess.run("docker stop emulator && docker rm emulator", shell=True).returncode, "failed")}
+        return {"status": {0: "completed"}.get(
+            subprocess.run("docker stop emulator && docker rm emulator", shell=True).returncode, "failed")}
 
-    @classmethod
-    def get_config_schema(cls):
-        if os.path.exists('Emulator/openapi/schema.json'):
-            return Response(json.dumps(cls._get_schema_file()), 200)
-        else:
-            return Response(json.dumps({"status": "Schema file does not exist"}), 200)
 
-    @classmethod
-    def _get_schema_file(cls):
-        with open('Emulator/openapi/schema.json', 'r') as schema_file:
-            return json.loads(schema_file.read())
