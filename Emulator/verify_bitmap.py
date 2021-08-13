10bc5eacb13140415072040e1ce0d019751651a4
Jim Matthews <jim.matthews@paconsulting.com>
2021-08-13 09:25:30 +0000

    Revert "Revert "use construct.Struct to parse bitmap header""
    This reverts commit 1e0db5920863502c0b122062ebf1d38d93ac2b54.

--- Emulator/verify_bitmap.py
+++ Emulator/verify_bitmap.py
@@ -1,12 +1,22 @@
 # Copyright PA Knowledge Ltd 2021
 # For licence terms see LICENCE.md file
+import construct
 
 
 class VerifyBitmap:
     @staticmethod
     def validate(bitmap):
-        return VerifyBitmap._verify_bitmap_type(bitmap)
+        return VerifyBitmap._check_valid_bitmap_header(bitmap)
 
     @staticmethod
-    def _verify_bitmap_type(bitmap):
-        return bitmap[:2] == b"BM"
+    def _check_valid_bitmap_header(data):
+        try:
+            VerifyBitmap._bitmap_header_bytes().parse(data)
+            return True
+        except construct.core.ConstructError as e:
+            return False
+
+    @staticmethod
+    def _bitmap_header_bytes():
+        return construct.Struct("Type" / construct.Const(b'BM'))
+
