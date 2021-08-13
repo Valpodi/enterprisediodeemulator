11132f7e36f2f4de319f50502bb791018de093a4
Jim Matthews <jim.matthews@paconsulting.com>
2021-08-13 08:06:15 +0000

    Revert "Revert "refactor diode_type file creation to python script""
    This reverts commit 3546be4634d21fd67cba28b45c1a60c12da91117.

--- scripts/runIntegrationTests.sh
+++ scripts/runIntegrationTests.sh
@@ -11,20 +11,23 @@ function cleanup()
 
 ./scripts/buildInterfaceAndEmulator.sh
 
+python3 test/write_diode_type.py -t basic
 pushd test
-echo '{"f2 type": "basic"}' > ../Emulator/config/diode_type.json
 export RESULT_FILENAME="emulator_test_results.xml"
 export PYTHON_SCRIPT="/tmp/emulator_tests.py"
 docker-compose -p emulator up --exit-code-from tester --build
 docker-compose -p emulator down
-echo '{"f2 type": "import"}' > ../Emulator/config/diode_type.json
+popd
+
+python3 test/write_diode_type.py -t import
+pushd test
 export RESULT_FILENAME="import_emulator_test_results.xml"
 export PYTHON_SCRIPT="/tmp/import_emulator_tests.py"
 docker-compose -p emulator up --exit-code-from tester --build
 docker-compose -p emulator down
 popd
 
-echo '{"f2 type": "basic"}' > Emulator/config/diode_type.json
+python3 test/write_diode_type.py -t basic
 ./scripts/buildInterfaceAndEmulator.sh
 python3 -m nose --with-xunit --xunit-file=test/verify_config_unit_test_results.xml Emulator/verify_config_tests.py
 python3 -m nose --with-xunit --xunit-file=test/verify_bitmap_unit_test_results.xml Emulator/verify_bitmap_tests.py
@@ -32,10 +35,10 @@ python3 -m nose --with-xunit --xunit-file=test/interface_unit_test_results.xml E
 python3 -m nose --with-xunit --xunit-file=test/interface_integration_test_results.xml test/mgmt_interface_integration_tests.py
 
 #python3 -m nose --with-xunit --xunit-file=test/emulator_port_span_test_results.xml test/emulator_port_span_test.py
-echo '{"f2 type": "basic"}' > Emulator/config/diode_type.json
+python3 test/write_diode_type.py -t basic
 python3 -m nose --with-xunit --xunit-file=test/e2e_test_results.xml test/e2e_test_interface_and_emulator.py
 
-echo '{"f2 type": "import"}' > Emulator/config/diode_type.json
+python3 test/write_diode_type.py -t import
 ./scripts/buildInterfaceAndEmulator.sh
 python3 -m nose --with-xunit --xunit-file=test/e2e_test_import_results.xml test/e2e_test_import_interface_and_emulator.py
-echo '{"f2 type": "basic"}' > Emulator/config/diode_type.json
+python3 test/write_diode_type.py -t basic
