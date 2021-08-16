66a9e2f17cc1241ddbc6ffe085487588ddaf4fe9
Jim Matthews <jim.matthews@paconsulting.com>
2021-08-16 12:48:23 +0000

    manually check bytes of header

--- Emulator/verify_bitmap.py
+++ Emulator/verify_bitmap.py
@@ -4,19 +4,14 @@ import construct
 
 
 class VerifyBitmap:
-    @staticmethod
-    def validate(bitmap):
-        return VerifyBitmap._check_valid_bitmap_header(bitmap)
+    def validate(self, bitmap):
+        return self._check_valid_bitmap_header(bitmap)
+
+    def _check_valid_bitmap_header(self, data):
+        return self._check_type(data)
 
     @staticmethod
-    def _check_valid_bitmap_header(data):
-        try:
-            VerifyBitmap._bitmap_header_bytes().parse(data)
+    def _check_type(data):
+        if data[:2] == b'BM':
             return True
-        except construct.core.ConstructError as e:
-            return False
-
-    @staticmethod
-    def _bitmap_header_bytes():
-        return construct.Struct("Type" / construct.Const(b'BM'))
-
+        return False
