0a1a772e8d2c9d3e914c80bf37eb69ec2ab5520c
Jim Matthews <jim.matthews@paconsulting.com>
2021-08-16 13:02:25 +0000

    use struct method to check bitmap file size bytes in header

--- Emulator/verify_bitmap.py
+++ Emulator/verify_bitmap.py
@@ -2,16 +2,35 @@
 # For licence terms see LICENCE.md file
 import construct
 
+#
+# class VerifyBitmap:
+#     def validate(self, bitmap):
+#         return self._check_valid_bitmap_header(bitmap)
+#
+#     def _check_valid_bitmap_header(self, data):
+#         return self._check_type(data)
+#
+#     @staticmethod
+#     def _check_type(data):
+#         if data[:2] == b'BM':
+#             return True
+#         return False
+
 
 class VerifyBitmap:
-    def validate(self, bitmap):
-        return self._check_valid_bitmap_header(bitmap)
-
-    def _check_valid_bitmap_header(self, data):
-        return self._check_type(data)
+    @staticmethod
+    def validate(bitmap):
+        return VerifyBitmap._check_valid_bitmap_header(bitmap)
 
     @staticmethod
-    def _check_type(data):
-        if data[:2] == b'BM':
+    def _check_valid_bitmap_header(data):
+        try:
+            VerifyBitmap._bitmap_header_bytes().parse(data)
             return True
-        return False
+        except construct.core.ConstructError as e:
+            return False
+
+    @staticmethod
+    def _bitmap_header_bytes():
+        return construct.Struct("Type" / construct.Const(b'BM'),
+                                "BF Size" / construct.Int32ub)
