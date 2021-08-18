72e3e40fde54b4db824885913813001384ca6f88
Jim Matthews <jim.matthews@paconsulting.com>
2021-08-18 13:39:17 +0000

    EOF byte can be 1 or 0

--- Emulator/verify_control_header.py
+++ Emulator/verify_control_header.py
@@ -9,22 +9,29 @@ class VerifyControlHeader:
     def validate(bitmap):
         try:
             VerifyControlHeader._check_valid_control_header(bitmap)
-            return True
         except InvalidControlHeaderError:
             return False
+
+        return VerifyControlHeader._check_EOF(bitmap)
 
     @staticmethod
     def _check_valid_control_header(data):
         try:
-            VerifyControlHeader._control_header_bytes().parse(data)
+            struct = VerifyControlHeader._control_header_bytes()
+            struct.parse(data)
         except construct.core.ConstructError:
             raise InvalidControlHeaderError("Invalid bitmap header")
 
     @staticmethod
     def _control_header_bytes():
         return construct.Struct("Frame_Count" / construct.Int32ul,
-                                "EOF" / construct.Const(b'\x01'),
-                                "Padding" / construct.Const(103*b'\x00'))
+                                "EOF" / construct.Int8ul,
+                                "Padding" / construct.Array(103, construct.Const(b'\x00')))
+
+    @staticmethod
+    def _check_EOF(data):
+        eof_byte = VerifyControlHeader._get_bitmap_header_field_by_name(data, "EOF")
+        return eof_byte == 0 or eof_byte == 1
 
     @staticmethod
     def _get_bitmap_header_field_by_name(data, field):
--- Emulator/verify_control_header_tests.py
+++ Emulator/verify_control_header_tests.py
@@ -8,9 +8,9 @@ from verify_control_header import VerifyControlHeader
 
 class VerifyControlHeaderTests(unittest.TestCase):
     control_header_as_dict = dict(Frame_Count=b'\x00\x00\x00\x00',
-                                  EOF=b'\x01',
+                                  EOF=b'\x00',
                                   Padding=103*b'\x00'
-                                 )
+                                  )
 
     def test_data_contains_valid_header(self):
         data_sample = b"".join(self.control_header_as_dict.values()) + b"\x00"
@@ -20,6 +20,12 @@ class VerifyControlHeaderTests(unittest.TestCase):
         invalid_frame_count_header = copy.deepcopy(self.control_header_as_dict)
         invalid_frame_count_header["Frame_Count"] = b'\x00\x00'
         data_sample = b"".join(invalid_frame_count_header.values())
+        self.assertFalse(VerifyControlHeader.validate(data_sample))
+
+    def test_control_header_returns_false_with_invalid_EOF(self):
+        invalid_eof_header = copy.deepcopy(self.control_header_as_dict)
+        invalid_eof_header["EOF"] = b'\x02'
+        data_sample = b"".join(invalid_eof_header.values())
         self.assertFalse(VerifyControlHeader.validate(data_sample))
 
 
